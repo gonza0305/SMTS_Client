ed4acdcfb010cbeac43a56f18f4b011e76c7f5c7
GeorgeMarkham <GeorgeMarkham@users.noreply.github.com>
2017-11-02 09:38:51 +0000

    Updated with code to write to socket

--- Connect.java
+++ Connect.java
@@ -1,13 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
-public class Connect {
+public class Main {
     public static void main(String[] args) {
         try {
             Socket client = new Socket("46.101.3.244", 5000);
-            PrintWriter out = new PrintWriter(client.getOutputStream());
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader sockIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedReader usrIn = new BufferedReader(new InputStreamReader(System.in));
+            String usrInput;
+            while((usrInput = usrIn.readLine()) != null){
+                out.println(usrInput);
+                System.out.println("Socket:" + sockIn.readLine());
+            }
         } catch (IOException exception){
             System.out.println(exception.getMessage());
         }
